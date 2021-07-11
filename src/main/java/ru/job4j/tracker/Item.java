8f01e4c35dbfc83c04b8de5026935d1b3556a713
Jazzik42 <rmuzrrr@gmail.com>
2021-07-11 20:53:16 +0500

    Исправил Item

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -6,17 +6,9 @@ import java.time.format.DateTimeFormatter;
 public class Item {
     private int id;
     private String name;
+    private LocalDateTime created = LocalDateTime.now();
     private static final DateTimeFormatter FORMATTER =
             DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
-    private LocalDateTime created = LocalDateTime.now();
-
-    public LocalDateTime getCreated() {
-        return created;
-    }
-
-    public void setCreated(LocalDateTime created) {
-        this.created = created;
-    }
 
     public Item() {
     }
@@ -28,6 +20,14 @@ public class Item {
     public Item(String name, int id) {
         this.name = name;
         this.id = id;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
+    }
+
+    public void setCreated(LocalDateTime created) {
+        this.created = created;
     }
 
     public void setName(String name) {
